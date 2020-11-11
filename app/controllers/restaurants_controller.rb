ef2e20f82227effc38d9954d36d94f6be668e07e
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-11 12:26:02 -0300

    Je commence le review controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,13 +16,15 @@ class RestaurantsController < ApplicationController
 
     def create
         @restaurant = Restaurant.new(restaurant_params)
-        @restaurant.save
-
-        redirect_to restaurant_path(@restaurant)
+        if @restaurant.save
+            redirect_to restaurant_path(@restaurant)
+        else
+            render :new
+        end
     end
 
     private
     def restaurant_params
-        params.require(:restaurant).permit(:name, :address, :phone_number)
+        params.require(:restaurant).permit(:name, :address, :phone_number, :category)
     end
 end
